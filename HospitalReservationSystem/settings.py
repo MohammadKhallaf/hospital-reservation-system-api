405f36ef8e9d3b63c3d311f1908a98503f4245cb
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-04-15 17:09:56 +0200

    Update deployment related settings

--- HospitalReservationSystem/settings.py
+++ HospitalReservationSystem/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
-
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -182,3 +182,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Heroku Additional settings
+django_on_heroku.settings(locals())
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: python manage.py migrate && gunicorn HospitalReservationSystem.wsgi --log-file -
+release: python manage.py migrate
+web: gunicorn HospitalReservationSystem.wsgi --log-file -
