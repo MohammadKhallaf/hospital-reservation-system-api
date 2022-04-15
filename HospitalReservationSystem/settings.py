652ef0ebdb48906b6ea9bb6c7ae614a88e2139d5
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-04-15 17:24:25 +0200

    Update database settings

--- HospitalReservationSystem/settings.py
+++ HospitalReservationSystem/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 import django_on_heroku
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -185,4 +187,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Heroku Additional settings
+DATABASE_URL = config("DATABASE_URL")
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 django_on_heroku.settings(locals())
