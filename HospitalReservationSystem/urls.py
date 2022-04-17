0032d7a6c3aaa0776ea6a5779695e3ef442dc8e4
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-17 16:27:50 +0200

    Code cleaning

--- HospitalReservationSystem/urls.py
+++ HospitalReservationSystem/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from re import template
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import routers
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -31,3 +30,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = "Fountain Valley Hospital Admin"
