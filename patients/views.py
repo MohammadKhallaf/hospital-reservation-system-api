9723c359fb33b5d25e4024e37c089e18a584d4d7
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-17 05:32:30 +0200

    Add authentication to the appointments end point

--- patients/views.py
+++ patients/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from patients.models import Appointment
 from patients.serializers import AppointmentSerializer
 
@@ -13,26 +13,29 @@ Appointments EndPoint
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def get_all_appointments(request):
     appointments = Appointment.objects.all()
     appointments_serializer = AppointmentSerializer(appointments, many=True)
-    print(appointments)
     return Response(appointments_serializer.data)
 
 
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def create_appointment(request):
-    appointment_serializer = AppointmentSerializer(data=request.data)
+    data = request.data
+    data["patient"] = request.user.id
+    appointment_serializer = AppointmentSerializer(data=data)
     if appointment_serializer.is_valid():
         appointment_serializer.save()
         return Response(appointment_serializer.data)
     return Response(appointment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["PUT"])
+@api_view(["PATCH"])
+@permission_classes([IsAuthenticated])
 def edit_appointment(request, pk):
     appointment = Appointment.objects.get(id=pk)
-    print(request.data)
     appointment_serializer = AppointmentSerializer(
         instance=appointment,
         data={"date": request.data["date"], "time": request.data["time"]},
@@ -45,6 +48,7 @@ def edit_appointment(request, pk):
 
 
 @api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
 def delete_appointment(request, pk):
     appointment = Appointment.objects.get(id=pk)
     appointment.delete()
