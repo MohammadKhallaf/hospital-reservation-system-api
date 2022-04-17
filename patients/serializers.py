9236b353cad3b92d5d6f52c833e129a04cf61837
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-04-17 05:34:50 +0200

    Update patients serializers

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,19 +1,18 @@
-from dataclasses import fields
-from pyexpat import model
 from rest_framework import serializers
-from doctors.serializers import DoctorSerializer
 
-import patients
 from .models import Appointment, Patient
 
-class PatientSerliazer(serializers.ModelSerializer):
+
+class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = "__all__"
+
 
 class AppointmentSerializer(serializers.ModelSerializer):
-    doctor = DoctorSerializer()
-    patient = PatientSerliazer()
+    doctor_id = serializers.ReadOnlyField()
+    patient_id = serializers.ReadOnlyField()
+
     class Meta:
         model = Appointment
-        fields ='__all__'
+        fields = "__all__"
